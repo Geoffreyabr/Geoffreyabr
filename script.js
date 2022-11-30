0f3dcfa283500ee7f21ffb8ea2f9719f5dcdd425
Geoffreyabr <geoffrey.abraham59@gmail.com>
2022-11-30 12:28:30 +0100

    working on html/css

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#form');
+const nomInput = document.querySelector('#nomInput');
+const listNoms = document.querySelector('#listNoms');
+
+form.onsubmit = function (event) {
+
+    event.preventDefault();
+
+const newList = document.createElement('li');
+newList.innerHTML = nomInput.value;
+listNoms.appendChild(newList);
+nomInput.value="";};
