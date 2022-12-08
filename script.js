f218415d708cb81317663499d9cf6556e15f5b06
Geoffreyabr <geoffrey.abraham59@gmail.com>
2022-12-08 08:43:43 +0100

    working on w3c error

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/* *****************liste de noms à proposer ************** */
-
-// const form = document.querySelector("#form");
-// const nomInput = document.querySelector("#nomInput");
-// const listNoms = document.querySelector("#listNoms");
-
-// form.onsubmit = function (event) {
-//   event.preventDefault();
-//   const newList = document.createElement("li");
-//   newList.innerHTML = nomInput.value;
-//   listNoms.appendChild(newList);
-//   nomInput.value = "";
-// };
-
-/* ************** fin de liste de noms **************************** */
-
 const icon = document.querySelector(".fa-bars");
 const list = document.querySelector(".navbarlist-mobile");
 
@@ -22,4 +6,26 @@ icon.addEventListener("click", function () {
   icon.classList.toggle("icon-visible");
 });
 
+
+
+/* *****************liste de noms à proposer ************** */
+
+const form = document.querySelector("#form");
+const nomInput = document.querySelector("#nomInput");
+const listNoms = document.querySelector("#listNoms");
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  const newList = document.createElement("li");
+  newList.innerHTML = nomInput.value;
+  listNoms.appendChild(newList);
+  nomInput.value = "";
+};
+
+console.log(form)
+
+
+
+
+/* ************** fin de liste de noms **************************** */
 
