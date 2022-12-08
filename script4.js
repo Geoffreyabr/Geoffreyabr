02994e3c0e9aee4656f99666873a195e657090f3
Geoffreyabr <geoffrey.abraham59@gmail.com>
2022-12-08 15:49:22 +0100

    adding alt

---
+++ script4.js
@@ -0,0 +1,7 @@
+/* ********************** ANIMATION H1 H2 ON LOADING ******************** */
+
+const firstTitle = document.querySelector('#mainh1');
+const secondTitle = document.querySelector('#mainh2');
+
+
+
